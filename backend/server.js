f39481b05fe054541c39cfd94d350f4dbe294916
Carlos Brito <cbrito1994@gmail.com>
2021-05-25 11:31:26 -0500

    Added the html routes to the server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTML Routes
+app.get('/tables', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.get('/reserve', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/notes.html'));
+});
+
+// If no matching route is found default to home
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // API Routes
 
