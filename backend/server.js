d4b476a991ec237458c3d6d52aa4f68597f969fd
Carlos Brito <cbrito1994@gmail.com>
2021-05-24 21:36:44 -0500

    Created the listening port and use the json method

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
 const express = require('express');
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// HTML Routes
+
+// API Routes
+
+// Listen
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
